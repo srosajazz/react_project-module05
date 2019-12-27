3e3f8d0d04d13f1ee6ed0857d0cb289ccf420be1
Sergio Rosa <scarlosjazz@gmail.com>
2019-12-27 09:25:30 -0500

    fix App.js Fragment

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
+import api from "../../services/api";
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-    return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+    async componentDidMount() {
+        const { match } = this.props;
+
+        const repoName = decodeURIComponent(match.params.repository);
+        /**
+         * call to the api for url:
+         *  https://github.com/microsoft/vscode/issues
+         *  const response = await api.get(`/repos/${repoName}`); // bad option
+         * const issues = await api.get(`/repos/${repoName}/issues`);
+         *
+         */
+        //better option for call all api
+        const [repository, issues] = await Promise.all([
+            api.get(`/repos/${repoName}`),
+            api.get(`/repos/${repoName}/issues`),
+        ]);
+
+        console.log(repository);
+        console.log(issues);
+    }
+    render() {
+        return <h1>Repository</h1>;
+    }
 }
